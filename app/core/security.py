819db512d97949054d6b488ce8beff337eea915c
mbabinot <malo.babinot@etudiant.univ-rennes.fr>
2026-03-11 20:42:18 +0100

    modification de la durée de validité du token

--- app/core/security.py
+++ app/core/security.py
@@ -19,7 +19,7 @@ load_dotenv()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 720))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 240))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
